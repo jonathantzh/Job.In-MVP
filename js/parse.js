6a4bc8afa26c04d7b662d47b0a987138a4e10990
Jon <jtan202@e.ntu.edu.sg>
2015-09-26 15:17:40 +0800

    sync

--- js/parse.js
+++ js/parse.js
@@ -1,16 +1,21 @@
 Parse.initialize("jYY9sPa7vefy9J3A1YxwVLfUTMRBVzIc9SefECZ7", "rrGAz3YXeI3Pijz2CmBum2gDIWpzUQDbsjpwvfSN");
 
-document.getElementById("submitEmail").addEventListener("click", function(){
+document.getElementById("submitForm").addEventListener("click", function(){
 submitEmail();
 })
 
 function submitEmail() {
-    var emailString = new Parse.Object("submitEmail");
-    emailString.set("email", document.getElementById("email").value);
+    var UserForm = new Parse.Object("UserForm");
+    UserForm.set("firstName", document.getElementById("InputFname").value);
+    UserForm.set("lastName", document.getElementById("InputLname").value);
+    UserForm.set("email", document.getElementById("InputEmailFirst").value);
+    UserForm.set("school", document.getElementById("InputSchool").value);
+    UserForm.set("study", document.getElementById("InputStudy").value);
+    UserForm.set("idealEnv", document.getElementById("InputEnv").value);
     
-    emailString.save({
+    UserForm.save({
         success: function (data) {
-            alert('Email Submitted!');
+            alert('Form Submitted!');
             console.log("success");
         },
         error: function (err) {
