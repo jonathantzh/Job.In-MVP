29d556edda277b54a492794a335e8c77c1896c40
Jonathan Tan <jonathan.x85@gmail.com>
2016-01-06 14:52:40 +0800

    search results return JSON

--- js/searchfunction.js
+++ js/searchfunction.js
@@ -5,19 +5,45 @@ require([
 
 document.getElementById("searchsubmit").addEventListener("click" , function() {
   var query = document.getElementById("searchvalue").value;
-  var results = idx.search(query).map(function (result) {
-    return questions.filter(function (q) { return q.id === parseInt(result.ref, 10);})[0];
-  });
-  $('#result').html(results);
-  console.log(results);
-});
 
-var questions = JSON.parse(smsdome).questions.map(function (raw) {
-  return {
-    title: raw.title,
-    body: raw.body,
-    industry: raw.industry
-  };
+switch (idx.search(query)[0].ref) {
+
+  case 1:
+  result1 = smsdome;
+  break;
+
+  case 2:
+  result1 = visenze;
+  break;
+
+  case 3:
+  result1 = vault;
+  break;
+
+}
+
+switch (idx.search(query)[1].ref) {
+
+  case 1:
+  result2 = smsdome;
+  break;
+
+  case 2:
+  result2 = visenze;
+  break;
+
+  case 3:
+  result2 = vault;
+  break;
+
+}
+
+  $('#result1').html(JSON.stringify(result1));
+  $('#result2').html(JSON.stringify(result2));
+
+  console.log(idx.search(query)[0]);
+  console.log(idx.search(query)[1]);
+  console.log(idx.search(query)[2]);
 });
 
 var idx = lunr(function () {
@@ -26,8 +52,11 @@ var idx = lunr(function () {
     this.field('industry', { boost: 8 });
 });
 
-idx.add(smsdome);
-idx.add(visenze);
+var employees = [
+    {"title":"John", "body":"Doe", "industry":"love", "id": 1},
+    {"title":"John", "body":"Doe", "industry":"love", "id": 2},
+    {"title":"John", "body":"Doe", "industry":"love", "id": 3}
+];
 
 var smsdome = {
     "title": "SmsDome",
@@ -36,11 +65,24 @@ var smsdome = {
     "id": 1
 };
 
+
 var visenze = {
-    "title": "Visenze",
+    "title": "Visenze Dragon",
     "body": "ViSenze is a Singapore-based startup providing visual search and visual image recognition for e-commerce and contextual advertising.",
-    "industry": "Digial Media Technology Image Recognition",
+    "industry": "Digital Media Technology Image Recognition SMS",
     "id": 2
 };
 
-});
+var vault = {
+    "title": "Vault Dragon",
+    "body": "Dubbed as the Dropbox for physical items, Vault Dragon offers a comprehensive storage solution. They deliver free storage boxes to the customers, and collect the boxes once the items are packed. They are stored securely until the customers request for a return.",
+    "industry": "Logistics Digital SMS",
+    "id": 3
+};
+
+idx.add(smsdome);
+idx.add(visenze);
+idx.add(vault);
+
+
+}); //end RequireJS
