81a41f6b8868c5c96137bf6951cbd83c3125977f
Jonathan Tan <jonathan.x85@gmail.com>
2016-01-09 01:44:57 +0800

    dynamic position changing!

---
+++ js/retrievepositions.js
@@ -0,0 +1,127 @@
+Parse.initialize("IfS2Dus2BdLSgzIQuxO6MU3Pr4Nw2zDanuhMJUQH", "H2mqcIuwNtgvApuiU9S7fcKCC3USWfgQwO1CRVsq");
+
+var SmsUser = Parse.Object.extend("User");
+var SmsQuery = new Parse.Query(SmsUser);
+query.get("P4W6oOTTdG", {
+  success: function(SmsQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#smspositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var CkUser = Parse.Object.extend("User");
+var CkQuery = new Parse.Query(CkUser);
+query.get("P4W6oOTTdG", {
+  success: function(CkQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#ckpositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var VisenzeUser = Parse.Object.extend("User");
+var VisenzeQuery = new Parse.Query(VisenzeUser);
+query.get("P4W6oOTTdG", {
+  success: function(VisenzeQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#visenzepositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var GoodrichUser = Parse.Object.extend("User");
+var GoodrichQuery = new Parse.Query(GoodrichUser);
+query.get("P4W6oOTTdG", {
+  success: function(GoodrichQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#smspositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var SmsUser = Parse.Object.extend("User");
+var SmsQuery = new Parse.Query(SmsUser);
+query.get("P4W6oOTTdG", {
+  success: function(SmsQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#smspositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var SmsUser = Parse.Object.extend("User");
+var SmsQuery = new Parse.Query(SmsUser);
+query.get("P4W6oOTTdG", {
+  success: function(SmsQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#smspositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var SmsUser = Parse.Object.extend("User");
+var SmsQuery = new Parse.Query(SmsUser);
+query.get("P4W6oOTTdG", {
+  success: function(SmsQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#smspositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var SmsUser = Parse.Object.extend("User");
+var SmsQuery = new Parse.Query(SmsUser);
+query.get("P4W6oOTTdG", {
+  success: function(SmsQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#smspositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+var SmsUser = Parse.Object.extend("User");
+var SmsQuery = new Parse.Query(SmsUser);
+query.get("P4W6oOTTdG", {
+  success: function(SmsQuery) {
+    // The object was retrieved successfully.
+    var opportunities = query.get("school");
+    $('#smspositions').html(opportunities);
+  },
+  error: function(object, error) {
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
